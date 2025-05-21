b83fd0859dd56155a3aa60ac3191a8969b98bf7b
sauk1346 <atmoralesc@gmail.com>
2025-05-21 19:02:10 -0400

    fix(data): remove hr04data data

--- src/data/hackerrank/Hr04Data.js
+++
@@ -1,8 +0,0 @@
-export const cppProblems = [
-    { 
-      id: "CPP001", 
-      title: "Say \"Hello, World!\" With C++", 
-      slug: "CPP001",
-      difficulty: "Easy",
-      category: "Introduction"
-    },

525b2fec790fa204a5a423238bf74fddcbd116ed
sauk1346 <atmoralesc@gmail.com>
2025-06-13 14:25:36 -0400

    update(leetcode): up to date

--- src/data/leetcode/problems.js
+++ src/data/leetcode/problems.js
@@ -7,61 +7,61 @@ export const problems = {
     },
     "LC0002": {
       title: "Suma de Dos Números",
-      topics: ["Listas Enlazadas", "Matemáticas", "Recursion"],
+      topics: ["Linked List", "Math", "Recursion"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC0009": {
       title: "Número Palíndromo",
-      topics: ["Matemáticas"],
+      topics: ["Math"],
       difficulty: "Easy",
       categories: ["general"]
     },
     "LC0075": {
       title: "Ordenar Colores",
-      topics: ["Arreglos", "Punteros", "Ordenamiento"],
+      topics: ["Array", "Two Pointers", "Sorting"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC0135": {
       title: "Dulces",
-      topics: ["Arreglos", "Algoritmo Voraz"],
+      topics: ["Array", "Greedy"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC0176": {
       title: "Segundo Salario más Alto",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC0177": {
       title: "N-ésimo Salario más Alto",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC0178": {
       title: "Clasificar Puntajes",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC0183": {
       title: "Clientes que Nunca Ordenan",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC0184": {
       title: "Salario Más Alto por Departamento",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC0196": {
       title: "Eliminar Emails Duplicados",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
@@ -73,253 +73,259 @@ export const problems = {
     },
     "LC0361": {
       title: "Bombardear Enemigos",
-      topics: ["Arreglos", "Programación Dinámica", "Matriz"],
+      topics: ["Array", "Dynamic Programming", "Matrix"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC0386": {
       title: "Números Lexicográficos",
-      topics: ["Búsqueda en Profundidad", "Árbol de prefijos"],
+      topics: ["Depth-First Searsh", "Trie"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC0595": {
       title: "Países Grandes",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC0909": {
       title: "Serpientes y Escaleras",
-      topics: ["Arreglos", "Búsqueda en Profundad", "Matriz"],
+      topics: ["Array", "Breadth-First Search", "Matrix"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC1061": {
       title: "Cadena Equivalente Lexicográficamente más Pequeña",
-      topics: ["Arreglos", "Conjuntos Disjuntos"],
+      topics: ["Array", "Union Find"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC1148": {
       title: "Visitas de Artículos I",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1173": {
       title: "Delivery de Comida Inmediata I",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1298": {
       title: "Máximo de Dulces a Sacar por Cajas",
-      topics: ["Arreglos", "Búsqueda por Amplitud", "Grafos"],
+      topics: ["Array", "Breadth-First Search", "Graph"],
       difficulty: "Hard",
       categories: ["General"]
     },
     "LC1322": {
       title: "Rendimiento de Anuncios",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1429": {
       title: "Primer Número Único",
-      topics: ["Arreglos", "Tablas Hash", "Diseño", "Cola", "Fluyo de Datos"],
+      topics: ["Array", "Hash Table", "Design", "Queue", "Data Stream"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC1517": {
       title: "Usuarios con Emails Válidos",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1527": {
       title: "Pacientes con una Condición",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1667": {
       title: "Corregir Nombres en una Tabla",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1683": {
       title: "Tweets No Válidos",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1757": {
       title: "Productos Reciclables y Bajos en Grasa",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1795": {
       title: "Reordenar Tabla de Productos",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1857": {
       title: "Color de Mayor Valor en un Grafo Dirigido",
-      topics: ["Tablas Hash", "Programación Dinámica", "Grafos", "Ordenamiento Topológico", "Memoización", "Conteo"],
+      topics: ["Hash Table", "Dynamic Programming", "Graph", "Topological Sort", "Memoization", "Counting"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC1873": {
       title: "Calcular Bono Especial",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC1907": {
       title: "Categorías de Salario",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC1931": {
       title: "Pintando una Cuadrícula con Tres Colores Diferentes",
-      topics: ["Programación Dinámica"],
-      difficulty: "Hard",
+      topics: ["Dynamic Programming"],
+Matrixdifficulty: "Hard",
       categories: ["general"]
     },
     "LC2082": {
       title: "Cantidad de Clientes Ricos",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Easy",
       categories: ["pandas"]
     },
     "LC2094": {
       title: "Encontrar Números Pares de 3 Dígitos",
-      topics: ["Arreglos", "Tablas Hash", "Ordenamiento", "Enumeración"],
+      topics: ["Array", "Hash Table", "Sorting", "Enumeration"],
       difficulty: "Easy",
       categories: ["general"]
     },
     "LC2131": {
       title: "Palíndromo Más Largo Contatenando Palabras de Dos Letras",
-      topics: ["Arreglos", "Tablas Hash", "Cadena de Caracteres", "Algoritmo Voraz", "Conteo"],
+      topics: ["Array", "Hash Table", "String", "Greedy", "Counting"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC2359": {
       title: "Find Closest Node to Given Two Nodes",
-      topics: ["Búsqueda en Profundidad","Grafos"],
+      topics: ["Depth-First Search","Graph"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC2434": {
       title: "Usando un Robot para Imprimir la Cadena Lexicográficamente más Pequeña",
-      topics: ["Tablas Hash", "Cadena de Caracteres", "Stack", "Algoritmo Voraz"],
+      topics: ["Hash Table", "String", "Stack", "Greedy"],
+      difficulty: "Medium",
+      categories: ["general"]
+    },
+    "LC2616": {
+      title: "Minimizar la Diferencia Máxima de Pares",
+      topics: ["Array", "Binary Search", "Greedy"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC2738": {
       title: "Contar Ocurrencias en el Texto",
-      topics: ["Base de Datos"],
+      topics: ["Database"],
       difficulty: "Medium",
       categories: ["pandas"]
     },
     "LC2894": {
       title: "Diferencia de Sumas Divisibles y No Divisibles",
-      topics: ["Matemáticas"],
+      topics: ["Math"],
       difficulty: "Easy",
       categories: ["general"]
     },
     "LC2900": {
       title: "Longest Unequal Adjacent Groups Subsequence I",
-      topics: ["Arreglos", "Cadena de Caracteres", "Programación Dinámica", "Algoritmo Voraz"],
+      topics: ["Array", "String", "Dynamic Programming", "Matrix"],
       difficulty: "Medium", 
       categories: ["general"]
     },
     "LC2901": {
       title: "Longest Unequal Adjacent Groups Subsequence II",
-      topics: ["Arreglos", "Cadena de Caracteres", "Programación Dinámica", "Algoritmo Voraz"],
+      topics: ["Array", "String", "Dynamic Programming", "Matrix"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC2927": {
       title: "Distribuir Dulces entre Niños III",
-      topics: ["Matemáticas", "Combinatoria"],
+      topics: ["Math", "Combinatorics"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC2929": {
       title: "Distribuir Dulces entre Niños II",
-      topics: ["Matemáticas", "Combinatoria", "Enumeración"],
+      topics: ["Math", "Combinatorics", "Enumeration"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC2942": {
       title: "Encuentra Palabras con un Carácter Específico",
-      topics: ["Arreglos", "Cadena de Caracteres"],
+      topics: ["Array", "String"],
       difficulty: "Easy",
       categories: ["general"]
     },
     "LC3024": {
       title: "Type of Triangle",
-      topics: ["Arreglos", "Matemáticas", "Ordenamiento"],
+      topics: ["Array", "Math", "Sorting"],
       difficulty: "Easy",
       categories: ["general"]
     },
     "LC3068": {
       title: "Encontrar la Suma Máxima de Valores de Nodos",
-      topics: ["Arreglos", "Programación Dinámica", "Algoritmo Voraz", "Manipulación de Bits", "Árbol", "Ordenamiento"],
+      topics: ["Array", "Dynamic Programming", "Greedy", "Bit Manipulation", "Tree", "Sorting"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC3170": {
       title: "Cadena Lexicográficamente Mínima después de Eliminar Estrellas",
-      topics: ["Tablas Hash", "Cadena de Caracteres", "Stack", "Algoritmo Voraz", "Heap"],
+      topics: ["Hash Table", "String", "Stack", "Greedy", "Heap"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC3335": {
-      title: "Total Characters in Cadena de Caracteres After Transformations I",
-      topics: ["Tablas Hash", "Matemáticas", "Cadena de Caracteres", "Programación Dinámica", "Conteo"],
+      title: "Total Characters in Strings After Transformations I",
+      topics: ["Hash Table", "Math", "String", "Dynamic Programming", "Matrix"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC3337": {
-      title: "Total Characters in Cadena de Caracteres After Transformations II",
-      topics: ["Tablas Hash", "Matemáticas", "Cadena de Caracteres", "Programación Dinámica", "Conteo"],
+      title: "Total Characters in Strings After Transformations II",
+      topics: ["Hash Table", "Math", "String", "Dynamic Programming", "Matrix"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC3355": {
       title: "Transformación a Arreglo-Cero I",
-      topics: ["Arreglos", "Suma Acumulada"],
+      topics: ["Array", "Prefix Sum"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC3362": {
       title: "Transformación a Arreglo-Cero III",
-      topics: ["Arreglos", "Algoritmo Voraz", "Ordenamiento", "Heap", "Suma Acumulada"],
+      topics: ["Array", "Greedy", "Sorting", "Heap", "Prefix Sum"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC3372": {
       title: "Maximizar Cantidad de Nodos Objetivos al Unir Árboles I",
-      topics: ["Árbol", "Búsqueda en Profundidad", "Búsqueda por Amplitud"],
+      topics: ["Tree", "Depth-First Search", "Breadth-First Search"],
       difficulty: "Medium",
       categories: ["general"]
     },
     "LC3373": {
       title: "Maximizar Cantidad de Nodos Objetivos al Unir Árboles II",
-      topics: ["Árbol", "Búsqueda en Profundidad", "Búsqueda por Amplitud"],
+      topics: ["Tree", "Depth-First Search", "Breadth-First Search"],
       difficulty: "Hard",
       categories: ["general"]
     },
     "LC3403": {
       title: "Encontrar la Cadena Lexicográficamente más Grande de la Caja I",
-      topics: ["Punteros", "Cadena de Caracteres", "Enumeración"],
+      topics: ["Two Pointers", "String", "Enumeration"],
       difficulty: "Medium",
       categories: ["general"]
     },

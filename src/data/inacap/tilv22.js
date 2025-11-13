f252d45eb58a9b1d7c16d9fe2ed9bb6dca5f19ed
sauk1346 <atmoralesc@gmail.com>
2025-11-13 14:10:24 -0300

    update(tilv22): add lectures 03-05

--- src/data/inacap/tilv22.js
+++ src/data/inacap/tilv22.js
@@ -87,15 +87,59 @@ const courseData = {
             date: 'Mié 29, Oct 2025', 
             description: [
                 {
-                    text: '',
-                    href: '',
+                    text: 'No asistí',
+                    strikethrough: true,
                 },
             ]
         },
         { 
             week: '02', 
-            class: '03', 
+            class: '04', 
             date: 'Jue 30, Oct 2025', 
+            description: [
+                {
+                    text: 'No asistí',
+                    strikethrough: true,
+                },
+            ]
+        },
+        { 
+            week: '03', 
+            class: '05', 
+            date: 'Mié 05, Nov 2025', 
+            description: [
+                {
+                    text: 'No asistí',
+                    strikethrough: true,
+                },
+            ]
+        },
+        { 
+            week: '03', 
+            class: '06', 
+            date: 'Jue 06, Oct 2025', 
+            description: [
+                {
+                    text: 'No asistí',
+                    strikethrough: true,
+                },
+            ]
+        },
+        { 
+            week: '04', 
+            class: '07', 
+            date: 'Mié 13, Nov 2025', 
+            description: [
+                {
+                    text: 'No asistí',
+                    strikethrough: true,
+                },
+            ]
+        },
+        { 
+            week: '04', 
+            class: '08', 
+            date: 'Jue 14, Nov 2025', 
             description: [
                 {
                     text: '',
@@ -129,6 +173,33 @@ const courseData = {
             date: 'Sáb 01, Nov 2025', 
             description: {
                 text: 'Desentrañando el ADN del videojuego: estructura, tecnología y diseño',
+                href: 'tilv22/lectura03',
+            }
+        },
+        { 
+            week: '03', 
+            lecture: '04', 
+            date: 'Sáb 08, Nov 2025', 
+            description: {
+                text: 'Arquitectura y Lógica de un Videojuego',
+                href: '/inacap/tilv22/docs/lectura04.pdf',
+            }
+        },
+        { 
+            week: '03', 
+            lecture: '05', 
+            date: 'Sáb 08, Nov 2025', 
+            description: {
+                text: 'Diseño de la Arquitectura del Videojuego',
+                href: 'tilv22/lectura05',
+            }
+        },
+        { 
+            week: '04', 
+            lecture: '06', 
+            date: 'Sáb 15, Nov 2025', 
+            description: {
+                text: 'Construyendo la Lógica del Videojuego',
                 href: '',
             }
         },
